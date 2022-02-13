dca5a44bcd4bcd868294208dd44bd6611a3a52f2
Balázs Sándor <sabuaai@INF.ELTE.HU>
2022-02-13 22:06:02 +0100

    Add colorting

---
+++ mandelbrot-2.1.py
@@ -0,0 +1,45 @@
+from math import log, log2
+from PIL import Image, ImageDraw
+
+MAX_ITER = 80
+
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < MAX_ITER:
+        z = z*z + c
+        n += 1
+
+    if n == MAX_ITER:
+        return MAX_ITER
+    
+    return n + 1 - log(log2(abs(z)))
+
+# Image size (pixels)
+WIDTH = 3000
+HEIGHT = 2000
+
+# Plot window
+RE_START = -2
+RE_END = 1
+IM_START = -1
+IM_END = 1
+
+im = Image.new('HSV', (WIDTH, HEIGHT), (0, 0, 0))
+draw = ImageDraw.Draw(im)
+
+for x in range(0, WIDTH):
+    for y in range(0, HEIGHT):
+        # Convert pixel coordinate to complex number
+        c = complex(RE_START + (x / WIDTH) * (RE_END - RE_START),
+                    IM_START + (y / HEIGHT) * (IM_END - IM_START))
+        # Compute the number of iterations
+        m = mandelbrot(c)
+        # The color depends on the number of iterations
+        hue = int(255 * m / MAX_ITER)
+        saturation = 255
+        value = 255 if m < MAX_ITER else 0
+        # Plot the point
+        draw.point([x, y], (hue, saturation, value))
+
+im.convert('RGB').save('output.png', 'PNG')
